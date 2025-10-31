2b4eaa968afeea4bf575617494950d3846e5671e
manukuiawa <manuelladefatima166@gmail.com>
2025-10-30 22:41:04 -0300

    Creating the classes person and missing persons anad instantiating the object

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,16 +8,16 @@ import model.Disappearence;
 public class Main {
 
 	public static void main(String[] args) {
-		// criando um objeto da classe Disappearence
+		
         Disappearence desaparecimento = new Disappearence(
             1, // idDisappearence
-            LocalDate.of(2025, 10, 30), // dateDisappearence
-            LocalTime.of(15, 45), // hourDisappearence
-            "Praia Central", // location
-            "Saiu para caminhar e não voltou", // contextDisappearence
-            "Camiseta branca e calça jeans", // clothesDisappearence
-            101, // idPeople (referência à pessoa)
-            5 // idCommunicant (quem comunicou)
+            LocalDate.of(2025, 10, 30), 
+            LocalTime.of(15, 45), 
+            "Praia Central", 
+            "Saiu para caminhar e não voltou", 
+            "Camiseta branca e calça jeans", 
+            101, 
+            5
         );
 
         System.out.println(desaparecimento);
