1667fca883ad8a795e9b2eb7206be6973a22778a
manukuiawa <manuelladefatima166@gmail.com>
2025-10-31 23:27:58 -0300

    Creating the Register Class and RegisterStatus as an Enum

--- src/model/People.java
+++ src/model/People.java
@@ -1,62 +1,63 @@
 package model;
 
 public class People {
-    private int idPeople; 
-    private String name; 
-    private int age; 
-    private String gender; 
-    private String CPF;
+	private int idPeople;
+	private String name;
+	private int age;
+	private String gender;
+	private String CPF;
 
-    public People() {
-    }
+	public People() {
+	}
 
-    public People(int idPeople, String name, int age, String gender, String CPF) {
-        this.idPeople = idPeople;
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-        this.CPF = CPF;
-    }
+	public People(int idPeople, String name, int age, String gender, String cPF) {
+		super();
+		this.idPeople = idPeople;
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		CPF = cPF;
+	}
 
-    public int getIdPeople() {
-        return idPeople;
-    }
+	public int getIdPeople() {
+		return idPeople;
+	}
 
-    public void setIdPeople(int idPeople) {
-        this.idPeople = idPeople;
-    }
+	public void setIdPeople(int idPeople) {
+		this.idPeople = idPeople;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public int getAge() {
-        return age;
-    }
+	public int getAge() {
+		return age;
+	}
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+	public void setAge(int age) {
+		this.age = age;
+	}
 
-    public String getGender() {
-        return gender;
-    }
+	public String getGender() {
+		return gender;
+	}
 
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
 
-    public String getCPF() {
-        return CPF;
-    }
+	public String getCPF() {
+		return CPF;
+	}
 
-    public void setCPF(String CPF) {
-        this.CPF = CPF;
-    }
+	public void setCPF(String CPF) {
+		this.CPF = CPF;
+	}
 
 	@Override
 	public String toString() {
---
+++ src/model/Register.java
@@ -0,0 +1,58 @@
+package model;
+
+import java.sql.Date;
+
+public class Register {
+
+	private int idRegister;
+	private Date dateReport;
+	private StatusRegister statusRegister;
+	private int idPeople;
+
+	public Register(int idRegister, Date dateReport, StatusRegister statusRegister, int idPeople) {
+		super();
+		this.idRegister = idRegister;
+		this.dateReport = dateReport;
+		this.statusRegister = statusRegister;
+		this.idPeople = idPeople;
+	}
+
+	public int getIdRegister() {
+		return idRegister;
+	}
+
+	public void setIdRegister(int idRegister) {
+		this.idRegister = idRegister;
+	}
+
+	public Date getDateReport() {
+		return dateReport;
+	}
+
+	public void setDateReport(Date dateReport) {
+		this.dateReport = dateReport;
+	}
+
+	public StatusRegister getStatusRegister() {
+		return statusRegister;
+	}
+
+	public void setStatusRegister(StatusRegister statusRegister) {
+		this.statusRegister = statusRegister;
+	}
+
+	public int getIdPeople() {
+		return idPeople;
+	}
+
+	public void setIdPeople(int idPeople) {
+		this.idPeople = idPeople;
+	}
+
+	@Override
+	public String toString() {
+		return "Register [idRegister=" + idRegister + ", dateReport=" + dateReport + ", statusRegister="
+				+ statusRegister + ", idPeople=" + idPeople + "]";
+	}
+
+}
