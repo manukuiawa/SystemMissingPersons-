b0aea48dce431c00f0b6b1809ae96ec9f4d23b16
manukuiawa <manuelladefatima166@gmail.com>
2025-11-06 15:53:04 -0300

    Creating screen components for registration - physicalCharacteristics done

--- src/view/RegisterPersonMissing.java
+++ src/view/RegisterPersonMissing.java
@@ -18,9 +18,11 @@ import javax.swing.JTextArea;
 public class RegisterPersonMissing {
 
 	private JFrame frame;
-	private JTextField inputName;
-	private JTextField inputAge;
+	private JTextField inputNome;
+	private JTextField inutIdade;
 	private JTextField inputLocalDesaparecido;
+	private JTextField inputAltura;
+	private JTextField inputPeso;
 
 	/**
 	 * Launch the application.
@@ -54,7 +56,7 @@ public class RegisterPersonMissing {
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		frame.setSize(732,646);
+		frame.setSize(732,803);
 		frame.setLocationRelativeTo(null);
 		frame.setResizable(false);
 		frame.getContentPane().setLayout(null);
@@ -66,46 +68,46 @@ public class RegisterPersonMissing {
 		titleMain.setBounds(26, 30, 541, 32);
 		frame.getContentPane().add(titleMain);
 		
-		JLabel lblNewLabel = new JLabel("Informações Pessoais");
-		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 15));
-		lblNewLabel.setForeground(new Color(4, 85, 191));
-		lblNewLabel.setBounds(28, 75, 183, 24);
-		frame.getContentPane().add(lblNewLabel);
+		JLabel titulo1Cadastro = new JLabel("Informações Pessoais");
+		titulo1Cadastro.setFont(new Font("Arial", Font.BOLD, 15));
+		titulo1Cadastro.setForeground(new Color(4, 85, 191));
+		titulo1Cadastro.setBounds(28, 75, 183, 24);
+		frame.getContentPane().add(titulo1Cadastro);
 		
 		JSeparator lineSeparator = new JSeparator();
 		lineSeparator.setForeground(new Color(255, 255, 255, 80));
 		lineSeparator.setBounds(26, 108, 274, 1);
 		frame.getContentPane().add(lineSeparator);
 		
-		JLabel LabelName = new JLabel("Nome Completo:");
-		LabelName.setFont(new Font("Arial", Font.PLAIN, 13));
-		LabelName.setForeground(new Color(255, 255, 255));
-		LabelName.setBackground(new Color(255, 255, 255));
-		LabelName.setBounds(26, 138, 113, 16);
-		frame.getContentPane().add(LabelName);
+		JLabel LabelNome = new JLabel("Nome Completo:");
+		LabelNome.setFont(new Font("Arial", Font.PLAIN, 13));
+		LabelNome.setForeground(new Color(255, 255, 255));
+		LabelNome.setBackground(new Color(255, 255, 255));
+		LabelNome.setBounds(26, 138, 113, 16);
+		frame.getContentPane().add(LabelNome);
 		
-		inputName = new JTextField();
-		inputName.setFont(new Font("Arial", Font.PLAIN, 14));
-		inputName.setBackground(new Color(140, 140, 140));
-		inputName.setBounds(26, 164, 274, 22);
-		frame.getContentPane().add(inputName);
-		inputName.setColumns(10);
-		inputName.setBorder(BorderFactory.createLineBorder(new Color(99,187,242), 1, true));
+		inputNome = new JTextField();
+		inputNome.setFont(new Font("Arial", Font.PLAIN, 14));
+		inputNome.setBackground(new Color(140, 140, 140));
+		inputNome.setBounds(26, 164, 274, 22);
+		frame.getContentPane().add(inputNome);
+		inputNome.setColumns(10);
+		inputNome.setBorder(BorderFactory.createLineBorder(new Color(99,187,242), 1, true));
 		
-		JLabel labelAge = new JLabel("Idade: ");
-		labelAge.setForeground(Color.WHITE);
-		labelAge.setFont(new Font("Arial", Font.PLAIN, 13));
-		labelAge.setBackground(Color.WHITE);
-		labelAge.setBounds(26, 216, 113, 16);
-		frame.getContentPane().add(labelAge);
+		JLabel labelidade = new JLabel("Idade: ");
+		labelidade.setForeground(Color.WHITE);
+		labelidade.setFont(new Font("Arial", Font.PLAIN, 13));
+		labelidade.setBackground(Color.WHITE);
+		labelidade.setBounds(26, 216, 113, 16);
+		frame.getContentPane().add(labelidade);
 		
-		inputAge = new JTextField();
-		inputAge.setFont(new Font("Arial", Font.PLAIN, 14));
-		inputAge.setColumns(10);
-		inputAge.setBorder(BorderFactory.createLineBorder(new Color(99,187,242), 1, true));
-		inputAge.setBackground(new Color(140, 140, 140));
-		inputAge.setBounds(26, 245, 113, 22);
-		frame.getContentPane().add(inputAge);
+		inutIdade = new JTextField();
+		inutIdade.setFont(new Font("Arial", Font.PLAIN, 14));
+		inutIdade.setColumns(10);
+		inutIdade.setBorder(BorderFactory.createLineBorder(new Color(99,187,242), 1, true));
+		inutIdade.setBackground(new Color(140, 140, 140));
+		inutIdade.setBounds(26, 245, 113, 22);
+		frame.getContentPane().add(inutIdade);
 		
 		JLabel labelSexo = new JLabel("Sexo:");
 		labelSexo.setForeground(Color.WHITE);
@@ -142,11 +144,11 @@ public class RegisterPersonMissing {
 		inputEndereco.setBounds(26, 409, 274, 65);
 		frame.getContentPane().add(inputEndereco);
 		
-		JLabel lblInformaesDoDesaparecimento = new JLabel("Informações do Desaparecimento");
-		lblInformaesDoDesaparecimento.setForeground(new Color(4, 85, 191));
-		lblInformaesDoDesaparecimento.setFont(new Font("Arial", Font.BOLD, 15));
-		lblInformaesDoDesaparecimento.setBounds(336, 75, 302, 24);
-		frame.getContentPane().add(lblInformaesDoDesaparecimento);
+		JLabel titulo2Cadastro = new JLabel("Informações do Desaparecimento");
+		titulo2Cadastro.setForeground(new Color(4, 85, 191));
+		titulo2Cadastro.setFont(new Font("Arial", Font.BOLD, 15));
+		titulo2Cadastro.setBounds(336, 75, 302, 24);
+		frame.getContentPane().add(titulo2Cadastro);
 		
 		JSeparator lineSeparator_1 = new JSeparator();
 		lineSeparator_1.setForeground(new Color(255, 255, 255, 80));
@@ -199,22 +201,94 @@ public class RegisterPersonMissing {
 		labelRegião.setBounds(336, 298, 113, 16);
 		frame.getContentPane().add(labelRegião);
 		
-		JComboBox inputSexo_1 = new JComboBox();
-		inputSexo_1.setModel(new DefaultComboBoxModel(new String[] {"Selecione a Opção: ", "Centro ", "Norte", "Sul", "Leste", "Oeste"}));
-		inputSexo_1.setBounds(336, 325, 352, 24);
-		frame.getContentPane().add(inputSexo_1);
+		JComboBox inputRegiao = new JComboBox();
+		inputRegiao.setModel(new DefaultComboBoxModel(new String[] {"Selecione a Opção: ", "Centro ", "Norte", "Sul", "Leste", "Oeste"}));
+		inputRegiao.setBounds(336, 325, 352, 24);
+		frame.getContentPane().add(inputRegiao);
 		
-		JLabel lblCircunstncias = new JLabel("Circunstâncias:");
-		lblCircunstncias.setForeground(Color.WHITE);
-		lblCircunstncias.setFont(new Font("Arial", Font.PLAIN, 13));
-		lblCircunstncias.setBackground(Color.WHITE);
-		lblCircunstncias.setBounds(336, 378, 113, 16);
-		frame.getContentPane().add(lblCircunstncias);
+		JLabel labelCircunstancias = new JLabel("Circunstâncias:");
+		labelCircunstancias.setForeground(Color.WHITE);
+		labelCircunstancias.setFont(new Font("Arial", Font.PLAIN, 13));
+		labelCircunstancias.setBackground(Color.WHITE);
+		labelCircunstancias.setBounds(336, 378, 113, 16);
+		frame.getContentPane().add(labelCircunstancias);
 		
 		JFormattedTextField inputCircunstancias = new JFormattedTextField();
 		inputCircunstancias.setText("Descreva as Circunstâncias do Desaparecimento");
 		inputCircunstancias.setBounds(336, 412, 352, 62);
 		frame.getContentPane().add(inputCircunstancias);
+		
+		JLabel lblCaractersticasFsicas = new JLabel("Características Físicas");
+		lblCaractersticasFsicas.setForeground(new Color(4, 85, 191));
+		lblCaractersticasFsicas.setFont(new Font("Arial", Font.BOLD, 15));
+		lblCaractersticasFsicas.setBounds(26, 511, 183, 24);
+		frame.getContentPane().add(lblCaractersticasFsicas);
+		
+		JSeparator lineSeparator_2 = new JSeparator();
+		lineSeparator_2.setForeground(new Color(255, 255, 255, 80));
+		lineSeparator_2.setBounds(26, 545, 662, 1);
+		frame.getContentPane().add(lineSeparator_2);
+		
+		inputAltura = new JTextField();
+		inputAltura.setFont(new Font("Arial", Font.PLAIN, 14));
+		inputAltura.setColumns(10);
+		inputAltura.setBorder(BorderFactory.createLineBorder(new Color(99,187,242), 1, true));
+		inputAltura.setBackground(new Color(140, 140, 140));
+		inputAltura.setBounds(26, 583, 130, 22);
+		frame.getContentPane().add(inputAltura);
+		
+		JLabel labelAltura = new JLabel("Altura: ");
+		labelAltura.setForeground(Color.WHITE);
+		labelAltura.setFont(new Font("Arial", Font.PLAIN, 13));
+		labelAltura.setBackground(Color.WHITE);
+		labelAltura.setBounds(26, 557, 113, 16);
+		frame.getContentPane().add(labelAltura);
+		
+		JLabel labelPeso = new JLabel("Peso: ");
+		labelPeso.setForeground(Color.WHITE);
+		labelPeso.setFont(new Font("Arial", Font.PLAIN, 13));
+		labelPeso.setBackground(Color.WHITE);
+		labelPeso.setBounds(166, 557, 113, 16);
+		frame.getContentPane().add(labelPeso);
+		
+		inputPeso = new JTextField();
+		inputPeso.setFont(new Font("Arial", Font.PLAIN, 14));
+		inputPeso.setColumns(10);
+		inputPeso.setBorder(BorderFactory.createLineBorder(new Color(99,187,242), 1, true));
+		inputPeso.setBackground(new Color(140, 140, 140));
+		inputPeso.setBounds(166, 583, 144, 22);
+		frame.getContentPane().add(inputPeso);
+		
+		JComboBox inputCordoCabelo = new JComboBox();
+		inputCordoCabelo.setModel(new DefaultComboBoxModel(new String[] {"Selecione a Opção: ", "Loiro ", "Castanho", "Preto", "Ruivo ", "Grisalho", "Careca"}));
+		inputCordoCabelo.setBounds(336, 582, 177, 24);
+		frame.getContentPane().add(inputCordoCabelo);
+		
+		JLabel labelCorDoCabelo = new JLabel("Cor do Cabelo:");
+		labelCorDoCabelo.setForeground(Color.WHITE);
+		labelCorDoCabelo.setFont(new Font("Arial", Font.PLAIN, 13));
+		labelCorDoCabelo.setBackground(Color.WHITE);
+		labelCorDoCabelo.setBounds(336, 559, 113, 16);
+		frame.getContentPane().add(labelCorDoCabelo);
+		
+		JLabel labelCorDosOlhos = new JLabel("Cor dos Olhos:");
+		labelCorDosOlhos.setForeground(Color.WHITE);
+		labelCorDosOlhos.setFont(new Font("Arial", Font.PLAIN, 13));
+		labelCorDosOlhos.setBackground(Color.WHITE);
+		labelCorDosOlhos.setBounds(523, 559, 113, 16);
+		frame.getContentPane().add(labelCorDosOlhos);
+		
+		JComboBox inputCordoCabelo_1 = new JComboBox();
+		inputCordoCabelo_1.setModel(new DefaultComboBoxModel(new String[] {"Selecione a Opção: ", "Castanhos ", "Azuis", "Verdes", "Pretos", "Mel "}));
+		inputCordoCabelo_1.setBounds(523, 582, 165, 24);
+		frame.getContentPane().add(inputCordoCabelo_1);
+		
+		JLabel labelRoupasNoDesaparecimento = new JLabel("Roupas no Desaparecimento: ");
+		labelRoupasNoDesaparecimento.setForeground(Color.WHITE);
+		labelRoupasNoDesaparecimento.setFont(new Font("Arial", Font.PLAIN, 13));
+		labelRoupasNoDesaparecimento.setBackground(Color.WHITE);
+		labelRoupasNoDesaparecimento.setBounds(26, 631, 185, 16);
+		frame.getContentPane().add(labelRoupasNoDesaparecimento);
 
 	
 	
