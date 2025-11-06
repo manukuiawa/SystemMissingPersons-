f70782ba27a1de70023b6e2697cbe6f3cc48f499
manukuiawa <manuelladefatima166@gmail.com>
2025-11-05 21:46:40 -0300

    Creating the name input on the missing person registration screen

--- src/view/RegisterPersonMissing.java
+++ src/view/RegisterPersonMissing.java
@@ -2,12 +2,18 @@ package view;
 
 import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
 
 public class RegisterPersonMissing {
 
 	private JFrame frame;
+	private JTextField inputName;
 
 	/**
 	 * Launch the application.
@@ -37,6 +43,7 @@ public class RegisterPersonMissing {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.getContentPane().setFont(new Font("Yu Gothic", Font.PLAIN, 18));
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
@@ -46,10 +53,41 @@ public class RegisterPersonMissing {
 		frame.getContentPane().setLayout(null);
 		frame.getContentPane().setBackground(Color.decode("#011826"));
 		
+		JLabel titleMain = new JLabel("Registrar Nova Pessoa Desaparecida");
+		titleMain.setFont(new Font("Arial", Font.BOLD, 25));
+		titleMain.setForeground(new Color(217, 4, 61));
+		titleMain.setBounds(26, 30, 541, 32);
+		frame.getContentPane().add(titleMain);
 		
+		JLabel lblNewLabel = new JLabel("Informações Pessoais");
+		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 15));
+		lblNewLabel.setForeground(new Color(4, 85, 191));
+		lblNewLabel.setBounds(28, 75, 183, 24);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JSeparator lineSeparator = new JSeparator();
+		lineSeparator.setForeground(new Color(255, 255, 255, 80));
+		lineSeparator.setBounds(26, 108, 326, 1);
+		frame.getContentPane().add(lineSeparator);
+		
+		JLabel LabelName = new JLabel("Nome Completo:");
+		LabelName.setFont(new Font("Arial", Font.PLAIN, 13));
+		LabelName.setForeground(new Color(255, 255, 255));
+		LabelName.setBackground(new Color(255, 255, 255));
+		LabelName.setBounds(26, 138, 113, 16);
+		frame.getContentPane().add(LabelName);
+		
+		inputName = new JTextField();
+		inputName.setFont(new Font("Arial", Font.PLAIN, 14));
+		inputName.setBackground(new Color(140, 140, 140));
+		inputName.setBounds(26, 164, 315, 22);
+		frame.getContentPane().add(inputName);
+		inputName.setColumns(10);
+		inputName.setBorder(BorderFactory.createLineBorder(new Color(99,187,242), 1, true));
+
+	
 	
 
 		
 	}
-
 }
