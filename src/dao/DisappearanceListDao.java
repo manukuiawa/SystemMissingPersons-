b0f15163a4a15e8ebd92c54eed9f565ddd288215
manukuiawa <manuelladefatima166@gmail.com>
2025-11-25 22:04:56 -0300

    Adding elements in DisappearanceListDao

--- src/dao/DisappearanceListDao.java
+++ src/dao/DisappearanceListDao.java
@@ -39,6 +39,8 @@ public class DisappearanceListDao extends BaseDao {
                 item.setHeight(rs.getDouble("height"));
                 item.setWeight(rs.getDouble("weight"));
                 item.setStatus(rs.getString("statusRegister"));
+                item.getStatus(statusRegister);
+                
 
                 lista.add(item);
             }
